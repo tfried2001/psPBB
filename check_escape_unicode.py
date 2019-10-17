76b7cb19fc0d8f8d819bda8089c47a2a84e8445d
tfried2001 <tfried_2001@yahoo.com>
2019-10-17 09:10:08 -0400

    M04 Start

--- check_escape_unicode.py
+++
@@ -1,8 +0,0 @@
-def check_unicode_conv(func):
-    def convert_unicode(*args, **kwargs):
-        return ascii(func(*args, **kwargs))
-    return convert_unicode
-
-@check_unicode_conv
-def print_unicode():
-    return 'ætna'
--- not_searched/path_test.py
+++
@@ -1,2 +0,0 @@
-def found():
-    print("Python found me!")
